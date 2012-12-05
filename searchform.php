4cda8f0ec78a84b49fa23d3f4ae5cb941676087a
Silvan Hagen <silvan@required.ch>
2012-12-05 10:41:13 +0100

    Removed more old textdomains

--- searchform.php
+++ searchform.php
@@ -8,13 +8,13 @@
 ?>
 <!-- START: searchform.php -->
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-	<label class="hide" for="s"><?php _e('Search for:', 'required'); ?></label>
+	<label class="hide" for="s"><?php _e('Search for:', 'requiredfoundation'); ?></label>
     <div class="row collapse">
     	<div class="eight mobile-three columns">
-        	<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'required'); ?>">
+        	<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'requiredfoundation'); ?>">
       	</div>
       	<div class="four mobile-one columns">
-        	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'required'); ?>" class="postfix button">
+        	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'requiredfoundation'); ?>" class="postfix button">
       	</div>
     </div>
 </form>
